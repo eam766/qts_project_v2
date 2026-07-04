140187af417e881fd3d6d0f4c45a2b74114741ab
eam766 <emilie.albert-moisan.1@ens.etsmtl.ca>
2026-07-04 19:22:48 -0400

    fix: logo email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src='https://raw.githubusercontent.com/eam766/qts_project/refs/heads/master/resources/js/assets/img/FrogLogo.png' alt="QTs Montréal" width="75">
-@else
-{{ $slot }}
-@endif
+<img src="{{ asset('images/FrogLogo.png') }}" alt="QTs Montréal" width="75">
 </a>
 </td>
 </tr>

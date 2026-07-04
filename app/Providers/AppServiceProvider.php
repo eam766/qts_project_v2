441ab8005cf25be2ac772ef5a1c63ec2b5898cbc
eam766 <emilie.albert-moisan.1@ens.etsmtl.ca>
2026-07-04 15:39:21 -0400

    Fix: forcer HTTPS pour éviter le mixed content en prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
-        
+{
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
